7682e0d4b482c14d3137d26bd3bad033c0924ed9
keyror <44626685+keymiro@users.noreply.github.com>
2026-05-20 16:13:22 -0500

    se agregan redes sociales

--- database/lookups/publish_channel.php
+++ database/lookups/publish_channel.php
@@ -5,6 +5,21 @@ return [
     ['category' => 'publish_channel', 'name' => 'Ciencuadras', 'alias' => 'CIENCUADRAS', 'code' => null],
     ['category' => 'publish_channel', 'name' => 'Metrocuadrado', 'alias' => 'METROCUADRADO', 'code' => null],
     ['category' => 'publish_channel', 'name' => 'Fincaraíz', 'alias' => 'FINCARAIZ', 'code' => null],
+
+    // Redes sociales
     ['category' => 'publish_channel', 'name' => 'Facebook', 'alias' => 'FACEBOOK', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Instagram', 'alias' => 'INSTAGRAM', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'TikTok', 'alias' => 'TIKTOK', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'X (Twitter)', 'alias' => 'X_TWITTER', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'LinkedIn', 'alias' => 'LINKEDIN', 'code' => null],
     ['category' => 'publish_channel', 'name' => 'WhatsApp', 'alias' => 'WHATSAPP', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Telegram', 'alias' => 'TELEGRAM', 'code' => null],
+
+    // Plataformas de video
+    ['category' => 'publish_channel', 'name' => 'YouTube', 'alias' => 'YOUTUBE', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Vimeo', 'alias' => 'VIMEO', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Dailymotion', 'alias' => 'DAILYMOTION', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Wistia', 'alias' => 'WISTIA', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Google Drive', 'alias' => 'GOOGLE_DRIVE', 'code' => null],
+    ['category' => 'publish_channel', 'name' => 'Dropbox', 'alias' => 'DROPBOX', 'code' => null],
 ];
