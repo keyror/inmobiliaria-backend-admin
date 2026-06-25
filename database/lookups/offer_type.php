041c7dde2a7d2e10937bc1194eb2390c21924d59
keyror <44626685+keymiro@users.noreply.github.com>
2026-06-25 11:32:28 -0500

    SE AJUSTAR SEEDER

--- database/lookups/offer_type.php
+++ database/lookups/offer_type.php
@@ -4,6 +4,5 @@ return [
     ['category' => 'offer_type', 'name' => 'Venta', 'alias' => 'VENTA', 'code' => null],
     ['category' => 'offer_type', 'name' => 'Arriendo', 'alias' => 'ARRIENDO', 'code' => null],
     ['category' => 'offer_type', 'name' => 'Transferencia', 'alias' => 'TRANSFERENCIA', 'code' => null],
-    ['category' => 'offer_type', 'name' => 'Venta y Arriendo', 'alias' => 'VENTA_Y_ARRIENDO', 'code' => null],
     ['category' => 'offer_type', 'name' => 'Hipoteca', 'alias' => 'HIPOTECA', 'code' => null],
 ];
